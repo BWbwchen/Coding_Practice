ef2092fee8f3477d1e06b02e93274313e27aeb25
BWbwchen <tim.chenbw@gmail.com>
2019-03-25 22:52:03 +0800

    3/25 finish 10954 11344 :)

--- 2019_spring/OJ/10954-Prefix_to_truth_table.c
+++ 2019_spring/OJ/10954-Prefix_to_truth_table.c
@@ -1,18 +1,78 @@
 #define DEBUG
+#define MAXN 100
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
+char expr[MAXN];
+int pos = 0;
 
+typedef struct _Node{
+    char ch;
+    struct _Node *left;
+    struct _Node *right;
+}Node;
+
+int cal(int A, int B, int C, int D){
+    int ans = 0, ex1 = 0, ex2 = 0;
+    if(pos < strlen(expr)){
+        char c = expr[pos++];
+     
+        if(c == '|'){
+            ex1 = cal(A, B, C, D);
+            ex2 = cal(A, B, C, D);
+            ans = (ex1 | ex2);
+        }else if(c == '&'){
+            ex1 = cal(A, B, C, D);
+            ex2 = cal(A, B, C, D);
+            ans = (ex1 & ex2);
+        }else if('A' <= c && c <= 'D'){
+            switch (c-'A'){
+                case 0:
+                    return A;
+                case 1:
+                    return B;
+                case 2:
+                    return C;
+                case 3:
+                    return D;
+                default:
+                    return 0;
+            }
+        }
+        return ans;
+    }
+}
+
+void print (Node *head){
+    if(head == NULL) return ;
+    print(head->left);
+    printf("%c ", head->ch);
+    print(head->right);
+
+}
 
 int main (){
     #ifdef DEBUG
     freopen("text.in", "r", stdin);
     #endif
     
-    
-	
-	
+    fgets(expr, MAXN-1, stdin);
+
+    for(int A = 0; A < 2; ++A){
+        for(int B = 0; B < 2; ++B){
+            for (int C = 0; C < 2; ++C){
+                for (int D = 0; D < 2; ++D){
+                    pos = 0;
+                    printf("%d %d %d %d %d\n", A, B, C, D, cal(A, B, C, D));
+                }
+            }
+        }
+    }
+
+
+
 
     return 0;
 }
--- 2019_spring/OJ/11344-Josephus_Problem_using_Circular_Linked_List.c
+++ 2019_spring/OJ/11344-Josephus_Problem_using_Circular_Linked_List.c
@@ -3,13 +3,57 @@
 #include <stdlib.h>
 #include <math.h>
 
+typedef struct node{
+    int data;
+    struct node *next;
+} Node;
 
+unsigned num = 0;
+
+Node* create(int n){
+    num = n;
+    int total = n+1;
+    Node *temp = NULL, *head = NULL;
+    head = (Node *)malloc(sizeof(Node ));
+    head->data = total-n; n--;
+    head->next = NULL;
+    temp = head;
+
+    while(n > 0){
+        Node *new = (Node *)malloc(sizeof(Node ));
+        temp->next = new;
+        new->data = total-n; n--;
+        new->next = NULL;
+        temp = new;
+    }
+    temp->next = head;
+
+    return head;
+
+}
+void josephus(int m, Node *head){
+    Node *now = head;
+    while(num != 1){
+        //find the node before target
+        for (int i = 1; i <= m-2; ++i) now = now->next;
+        Node *del = now->next;
+        printf("%d ", del->data);
+        num--;
+        now->next = now->next->next;
+        now = now->next;
+        free(del);
+    }
+    printf("%d ", now->data);
+}
 
 int main (){
     #ifdef DEBUG
     freopen("text.in", "r", stdin);
     #endif
     
+	int n,m;
+	scanf("%u %u",&n, &m);
+	josephus(m,create(n));
     
 	
 	

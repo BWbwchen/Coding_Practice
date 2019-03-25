d3be5b437d1df2a8381db036a75ddc3eaa65835a
BWbwchen <tim.chenbw@gmail.com>
2019-03-25 13:30:50 +0800

    3/25 i change to elemantary os :) excellent

---
+++ 2019_spring/OJ/10966-Infix_to_syntax_tree.c
@@ -0,0 +1,18 @@
+#define DEBUG
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+
+int main (){
+    #ifdef DEBUG
+    freopen("text.in", "r", stdin);
+    #endif
+    
+    
+	
+	
+
+    return 0;
+}

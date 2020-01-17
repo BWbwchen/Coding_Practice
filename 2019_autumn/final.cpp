4b1804eefcd4e336e3eb3ee165da41df6603ad07
BWbwchen <tim.chenbw@gmail.com>
2020-01-17 23:14:16 +0800

    1/17 finish kcore

--- 2019_autumn/final.cpp
+++ 2019_autumn/final.cpp
@@ -4,14 +4,44 @@
 
 using namespace std;
 
-unsigned long int vertex = 5;
+unsigned long int vertex = 8;
 class solution {
    private:
     bool **map;
 
+    set<INDEX> ans_set(set<INDEX> s, INDEX v) {
+        // return s and neibor(v) 
+        set<INDEX> to_return;
+        for (INDEX i = 0; i < vertex; ++i) {
+            if (map[v][i] && s.find(i) != s.end()) to_return.insert(i);
+        }
+        return to_return;
+    }
+    void clique(set<INDEX> a, set<INDEX> p, set<INDEX> x) {
+        if (p.size() == 0 && x.size() == 0) {
+            for (set<INDEX>::iterator it = a.begin(); it != a.end(); ++it) {
+                cout << *it << " ";
+            }
+            cout << endl;
+            return;
+        }
+        for (INDEX i = 0; i < vertex; ++i) {
+            if (p.find(i) != p.end()) {
+                set<INDEX> new_a = a;
+                new_a.insert(i);
+                set<INDEX> new_p = ans_set(p, i);
+                set<INDEX> new_x = ans_set(x, i);
+
+                clique(new_a, new_p, new_x);
+                p.erase(i);
+                x.insert(i);
+            }
+        }
+    }
+
    public:
     solution() {
-        map = new bool*[vertex];
+        map = new bool *[vertex];
         for (INDEX i = 0; i < vertex; ++i) {
             map[i] = new bool[vertex];
         }
@@ -20,7 +50,6 @@ class solution {
                 map[i][j] = false;
             }
         }
-        //for (INDEX i = 0; i < vertex; ++i) map[i][i] = true;
     }
     void input() {
         INDEX a, b;
@@ -30,42 +59,44 @@ class solution {
         }
     }
     void clique_starter() {
+        //freopen("clique.txt", "w", stdout);
         set<INDEX> a, p, x;
         for (INDEX i = 0; i < vertex; ++i) {
             p.insert(i);
         }
         clique(a, p, x);
     }
-    set<INDEX> ans_set(set<INDEX> s, set<INDEX> ans, INDEX v) {
-        set<INDEX> newset = s;
-        // check if it conneted to all node in answer set
-        for (set<INDEX>::iterator it = ans.begin(); it != ans.end(); ++it) {
-            if (!map[v][*it]) return s;
-        }
-        newset.insert(v);
-        return newset;
-    }
-    void clique(set<INDEX> a, set<INDEX> p, set<INDEX> x) {
-        if (p.size() == 0 ) {
-            for (set<INDEX>::iterator it = a.begin(); it != a.end(); ++it) {
-                cout << *it << " ";
-            }
-            cout << endl;
-            return;
-        }
+    void k_core () {
+        //freopen("k_core.txt", "w", stdout);
+        vector<INDEX> core(vertex, 0);
+        // computer the degrees of vertices
+        // {degree, index}
+        vector<pair<INDEX, INDEX>> degree(vertex, {0, 0});
         for (INDEX i = 0; i < vertex; ++i) {
-            if (p.find(i) != p.end() && x.find(i) == x.end()) {
-                set<INDEX> new_a = ans_set(a, a, i);
-                set<INDEX> new_p = p;
-                set<INDEX> new_x = ans_set(x, a, i);
-
-                new_p.erase(i);
-
-                clique(new_a, new_p, new_x);
-                //p.erase(i);
-                //x.insert(i);
-                if (new_a.find(i) != new_a.end()) x.insert(i); 
+            degree[i].second = i;
+            for (INDEX j = 0; j < vertex; ++j) {
+                if (map[i][j]) 
+                    degree[i].first++;
             }
+        }
+        // order the set increasing by degree
+        sort(degree.begin(), degree.end());
+
+        for (INDEX i = 0; i < vertex; ++i) {
+            core[degree[i].second] = degree[i].first;
+            for (INDEX j = 0; j < vertex; ++j) {
+                if (map[i][j]) {
+                    if (degree[j].first > degree[i].first) {
+                        degree[j].first--;
+                        sort(degree.begin()+j, degree.end());
+                    }
+                }
+            }
+        }
+
+        // print the answer
+        for (INDEX i = 0; i < vertex; ++i) {
+            cout << i << " " << core[i] << endl;
         }
     }
 };
@@ -77,6 +108,7 @@ int main(int argc, char **argv) {
     solution t;
     t.input();
     t.clique_starter();
+    t.k_core();
 
     return 0;
 }

926514af70d3f5df33812e69ae645121453107a8
BWbwchen <tim.chenbw@gmail.com>
2020-01-18 20:00:49 +0800

    1/18 clique done

--- 2019_autumn/final.cpp
+++ 2019_autumn/final.cpp
@@ -1,5 +1,6 @@
 #define DEBUG
 #define INDEX unsigned long int
+#define TYPE set
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -10,32 +11,39 @@ class solution {
    private:
     bool **map;
     INDEX kcore;
-    set<INDEX> max_clique;
+    TYPE<INDEX> max_clique;
 
-    set<INDEX> ans_set(set<INDEX> s, INDEX v) {
+    TYPE<INDEX> ans_set(TYPE<INDEX> s, INDEX v) {
         // return s and neibor(v)
-        set<INDEX> to_return;
-        for (INDEX i = 0; i < vertex; ++i) {
-            if (map[v][i] && s.find(i) != s.end()) to_return.insert(i);
+        TYPE<INDEX> to_return;
+        for (TYPE<INDEX>::iterator it = s.begin();
+             it != s.end(); ++it) {
+            if (map[v][*it]) to_return.insert(*it);
         }
         return to_return;
     }
-    void clique(set<INDEX> a, set<INDEX> p, set<INDEX> x) {
+    void clique(TYPE<INDEX> a, TYPE<INDEX> p, TYPE<INDEX> x) {
         if (p.size() == 0 && x.size() == 0) {
             if (a.size() > max_clique.size()) max_clique = a;
             return;
         }
-        for (INDEX i = 0; i < vertex; ++i) {
-            if (p.find(i) != p.end()) {
-                set<INDEX> new_a = a;
-                new_a.insert(i);
-                set<INDEX> new_p = ans_set(p, i);
-                set<INDEX> new_x = ans_set(x, i);
 
-                clique(new_a, new_p, new_x);
-                p.erase(i);
-                x.insert(i);
-            }
+        TYPE<INDEX> old_p = p;
+        // try with pivot
+        INDEX pivot_u;
+        if (p.size() == 0) pivot_u = *(x.begin());
+        else pivot_u = *(p.begin());
+        for (TYPE<INDEX>::iterator it = p.begin();
+            it != p.end(); ++it) {
+            if (map[*it][pivot_u]) continue;
+            TYPE<INDEX> new_a = a;
+            if (new_a.find(*it) == new_a.end()) new_a.insert(*it);
+            TYPE<INDEX> new_p = ans_set(p, *it);
+            TYPE<INDEX> new_x = ans_set(x, *it);
+
+            clique(new_a, new_p, new_x);
+            if (p.find(*it) != p.end()) p.erase(it);
+            if (x.find(*it) == x.end()) x.insert(*it);
         }
     }
 
@@ -64,13 +72,14 @@ class solution {
     }
     void clique_starter() {
         // freopen("clique.txt", "w", stdout);
-        set<INDEX> a, p, x;
+        TYPE<INDEX> a, p, x;
         for (INDEX i = 0; i < vertex; ++i) {
             p.insert(i);
         }
         clique(a, p, x);
-
-        for (set<INDEX>::iterator it = max_clique.begin();
+    }
+    void clique_ans() {
+        for (TYPE<INDEX>::iterator it = max_clique.begin();
              it != max_clique.end(); ++it) {
             cout << *it << endl;
         }
@@ -83,7 +92,7 @@ class solution {
         }
     }
     void k_core() {
-        freopen("k_core.txt", "w", stdout);
+//        freopen("k_core.txt", "w", stdout);
         vector<INDEX> core(vertex, 0);
         // computer the degrees of vertices
         // {degree, index}
@@ -119,17 +128,22 @@ class solution {
 
 int main(int argc, char **argv) {
 #ifdef DEBUG
-    //freopen("in.in", "r", stdin);
+//    freopen("in.in", "r", stdin);
     freopen("sample_input.txt", "r", stdin);
-    //freopen("open_testcase_ds.txt", "r", stdin);
+    // freopen("open_testcase_ds.txt", "r", stdin);
 #endif
     solution t;
     t.input();
     clock_t start2 = clock();
     t.clique_starter();
+    double time = ((double)(clock() - start2));
+    t.clique_ans();
+    cout << "Time (precise) = " << time / CLOCKS_PER_SEC << endl;
+    /*
+    t.k_core();
     cout << "Time (precise) = " << ((double)(clock() - start2)) / CLOCKS_PER_SEC
          << endl;
-    t.k_core();
+    */
 
     return 0;
 }

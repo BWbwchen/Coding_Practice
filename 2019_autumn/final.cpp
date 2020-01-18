63e66220d93556260dddca9a67d3f85918ee8449
BWbwchen <tim.chenbw@gmail.com>
2020-01-18 14:28:50 +0800

    1/18 QQ

--- 2019_autumn/final.cpp
+++ 2019_autumn/final.cpp
@@ -5,14 +5,15 @@
 using namespace std;
 
 unsigned long int vertex = 500;
-//unsigned long int vertex = 5;
+// unsigned long int vertex = 5;
 class solution {
    private:
     bool **map;
     INDEX kcore;
+    set<INDEX> max_clique;
 
     set<INDEX> ans_set(set<INDEX> s, INDEX v) {
-        // return s and neibor(v) 
+        // return s and neibor(v)
         set<INDEX> to_return;
         for (INDEX i = 0; i < vertex; ++i) {
             if (map[v][i] && s.find(i) != s.end()) to_return.insert(i);
@@ -21,10 +22,7 @@ class solution {
     }
     void clique(set<INDEX> a, set<INDEX> p, set<INDEX> x) {
         if (p.size() == 0 && x.size() == 0) {
-            for (set<INDEX>::iterator it = a.begin(); it != a.end(); ++it) {
-                cout << *it << " ";
-            }
-            cout << endl;
+            if (a.size() > max_clique.size()) max_clique = a;
             return;
         }
         for (INDEX i = 0; i < vertex; ++i) {
@@ -63,31 +61,28 @@ class solution {
             map[a][b] = true;
             map[b][a] = true;
         }
-        /*
-        cout << "this is map \n";
-        for (INDEX i = 0; i < vertex; ++i) {
-            for (INDEX j = 0; j < vertex; ++j) {
-                cout << map[i][j] << " ";
-            }
-            cout << endl;
-        }
-        */
     }
     void clique_starter() {
-        freopen("clique.txt", "w", stdout);
+        // freopen("clique.txt", "w", stdout);
         set<INDEX> a, p, x;
         for (INDEX i = 0; i < vertex; ++i) {
             p.insert(i);
         }
         clique(a, p, x);
-    }
-    void debug (vector<pair<INDEX, INDEX>> a) {
-        cout << "---------------debug-------------" << endl;
-        for (INDEX i = 0; i < a.size(); ++i) {
-            cout << "core number is " << a[i].second << " core is " << a[i].first << endl;
+
+        for (set<INDEX>::iterator it = max_clique.begin();
+             it != max_clique.end(); ++it) {
+            cout << *it << endl;
         }
     }
-    void k_core () {
+    void debug(vector<pair<INDEX, INDEX>> a) {
+        cout << "---------------debug-------------" << endl;
+        for (INDEX i = 0; i < a.size(); ++i) {
+            cout << "core number is " << a[i].second << " core is "
+                 << a[i].first << endl;
+        }
+    }
+    void k_core() {
         freopen("k_core.txt", "w", stdout);
         vector<INDEX> core(vertex, 0);
         // computer the degrees of vertices
@@ -96,24 +91,20 @@ class solution {
         for (INDEX i = 0; i < vertex; ++i) {
             degree[i].second = i;
             for (INDEX j = 0; j < vertex; ++j) {
-                if (map[i][j]) 
-                    degree[i].first++;
+                if (map[i][j]) degree[i].first++;
             }
         }
         // order the set increasing by degree
         sort(degree.begin(), degree.end());
 
         for (INDEX i = 0; i < vertex; ++i) {
-            //cout << "core " << degree[i].second << " is " << degree[i].first << endl;
             core[degree[i].second] = degree[i].first;
             for (INDEX j = 0; j < vertex; ++j) {
-                //printf("map[%d][%d] is : %d\n", degree[i].second, j, map[degree[i].second][j]);
                 // adjcent
                 if (map[degree[i].second][degree[j].second]) {
                     if (degree[j].first > degree[i].first) {
                         degree[j].first--;
-                        sort(degree.begin()+i+1, degree.end());
-                        //debug(degree);
+                        sort(degree.begin() + i + 1, degree.end());
                     }
                 }
             }
@@ -130,10 +121,14 @@ int main(int argc, char **argv) {
 #ifdef DEBUG
     //freopen("in.in", "r", stdin);
     freopen("sample_input.txt", "r", stdin);
+    //freopen("open_testcase_ds.txt", "r", stdin);
 #endif
     solution t;
     t.input();
+    clock_t start2 = clock();
     t.clique_starter();
+    cout << "Time (precise) = " << ((double)(clock() - start2)) / CLOCKS_PER_SEC
+         << endl;
     t.k_core();
 
     return 0;

be92bc47115b52b8640ffec2c85f0dce09de1ec6
BWbwchen <tim.chenbw@gmail.com>
2021-09-22 09:53:57 +0800

    add leetcode 215.cpp priority queue method

--- leetcode/215.cpp
+++ leetcode/215.cpp
@@ -6,3 +6,15 @@ public:
         return nums[k-1];
     }
 };
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int> > q;
+        for (auto v : nums) {
+            q.push(v);
+            if (q.size() > k) q.pop();
+        }
+        return q.top();
+    }
+};

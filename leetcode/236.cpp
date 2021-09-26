de8596a8e5a97aa31971984bb2ec829a260214bc
BWbwchen <tim.chenbw@gmail.com>
2021-09-26 09:13:04 +0800

    add leetcode 236.cpp

---
+++ leetcode/236.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+// What if p or q isn't in the tree?
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root == nullptr || root == p || root == q) return root;
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        
+        if (left != nullptr && right != nullptr) {
+            return root; 
+        } else if (left != nullptr) {
+            return left;
+        } else if (right != nullptr) {
+            return right;
+        } else {
+            return nullptr;
+        }
+    }
+};

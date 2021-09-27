6b950cc7bd6e80bd95047552e827a55a522e6202
BWbwchen <tim.chenbw@gmail.com>
2021-09-27 09:08:31 +0800

    add leetcode 240.cpp, awesome BST

---
+++ leetcode/240.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int i = 0;
+        int j = matrix[0].size()-1;
+        
+        while (i < matrix.size() && j >= 0) {
+            if (matrix[i][j] < target) {
+                i++;
+            } else if (matrix[i][j] > target) {
+                j--;
+            } else {
+                return true; 
+            }
+        }
+        return false;
+    }
+};

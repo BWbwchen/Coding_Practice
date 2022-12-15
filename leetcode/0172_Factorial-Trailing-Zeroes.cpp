98e0d3e7b39df3d794a5f15b38b587835d76e16c
BWbwchen <tim.chenbw@gmail.com>
2022-12-15 19:25:24 +0800

    [feat   ]: add leetcode 172
    Signed-off-by: Bo-Wei Chen(BWbwchen) <tim.chenbw@gmail.com>

---
+++ leetcode/0172_Factorial-Trailing-Zeroes.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int ans = 0;
+        while (n > 0) {
+            ans += n / 5;
+            n /= 5;
+        }
+        return ans;
+    }
+};

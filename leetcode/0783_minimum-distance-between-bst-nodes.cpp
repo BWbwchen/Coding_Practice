356857e854b805350e368bffcd543da2f0e5bca6
BWbwchen <tim.chenbw@gmail.com>
2023-06-14 09:54:00 +0800

    [feat   ]: leetcode 783, same as 530
    Signed-off-by: Bo-Wei Chen(BWbwchen) <tim.chenbw@gmail.com>

---
+++ leetcode/0783_minimum-distance-between-bst-nodes.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int minDiffInBST(TreeNode *root) {
+        // bfs
+        vector<int> d;
+        queue<TreeNode *> q;
+        q.emplace(root);
+
+        while (!q.empty()) {
+            auto top = q.front();
+            if (top == nullptr) {
+                continue;
+            }
+
+            q.pop();
+
+            d.emplace_back(top->val);
+            if (top->left)
+                q.emplace(top->left);
+            if (top->right)
+                q.emplace(top->right);
+        }
+
+        // find minimum difference
+        int n = d.size();
+        int ret = numeric_limits<int>::max();
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                ret = min(ret, abs(d[i] - d[j]));
+            }
+        }
+        return ret;
+    }
+};

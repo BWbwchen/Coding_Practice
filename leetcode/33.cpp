2999a4e46451ca618b7aacb9fcfa7b5c563759e7
BWbwchen <tim.chenbw@gmail.com>
2023-08-12 16:03:19 +0800

    [opt    ]: leetcode 33
    Signed-off-by: Bo-Wei Chen(BWbwchen) <tim.chenbw@gmail.com>

--- leetcode/33.cpp
+++ leetcode/33.cpp
@@ -1,7 +1,7 @@
 // O(n) but faster than 96.48% solution ???
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
+    int search(vector<int> &nums, int target) {
         for (int i = 0; i < nums.size(); ++i) {
             if (target == nums[i]) {
                 return i;
@@ -15,13 +15,13 @@ public:
 // Use binary search, O(logn) but much slower why ???
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int start = 0, end = nums.size()-1;
+    int search(vector<int> &nums, int target) {
+        int start = 0, end = nums.size() - 1;
         int mid;
         while (start < end) {
-            mid = start + (end - start)/2;
+            mid = start + (end - start) / 2;
             if (nums[mid] > nums[end]) {
-                // start ~ mid is sorted 
+                // start ~ mid is sorted
                 if (target == nums[mid]) {
                     return mid;
                 } else if (target > nums[mid] || target <= nums[end]) {
@@ -41,7 +41,47 @@ public:
             }
         }
         // return start
-        if (nums[start] == target) return start;
+        if (nums[start] == target)
+            return start;
+        return -1;
+    }
+};
+
+// Binary search with less search step
+// Key point is to find which trend that target in. ascending part? or
+// descending part?
+class Solution {
+public:
+    int search(vector<int> &nums, int target) {
+        int l = 0, r = nums.size() - 1;
+        int mid = -1;
+
+        while (l <= r) {
+            mid = l + ((r - l) >> 1);
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[l] == target) {
+                return l;
+            } else if (nums[r] == target) {
+                return r;
+            } else if (nums[mid] > nums[r]) {
+                if (nums[mid] == target) {
+                    return mid;
+                } else if (nums[mid] > target && target > nums[l]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            } else {
+                if (nums[mid] == target) {
+                    return mid;
+                } else if (nums[mid] < target && target < nums[r]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+        }
         return -1;
     }
 };

0e6bf2ce39bce4714911e81cfdd5f04c38c85717
BWbwchen <tim.chenbw@gmail.com>
2023-11-17 08:50:45 +0800

    [feat   ]: leetcode 1 with rust FP style
    Signed-off-by: Bo-Wei Chen(BWbwchen) <tim.chenbw@gmail.com>

--- leetcode/0001_Two-Sum.rs
+++ leetcode/0001_Two-Sum.rs
@@ -14,3 +14,26 @@ impl Solution {
         unreachable!()
     }
 }
+
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        type TAcc = (Vec<i32>, HashMap<i32, i32>);
+
+        let mut init = (Vec::with_capacity(2), HashMap::with_capacity(nums.len()));
+        nums.iter()
+            .enumerate()
+            .fold(init, |mut acc, (i, &v)| {
+                let find = target - v;
+                if let Some(&x) = acc.1.get(&find) {
+                    acc.0 = vec![i as i32, x];
+                } else {
+                    acc.1.insert(v, i as i32);
+                }
+
+                acc
+            })
+            .0
+    }
+}

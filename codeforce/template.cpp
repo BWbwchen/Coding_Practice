cd77e429f246debb6528152956b056a5f8617a45
BWbwchen <tim.chenbw@gmail.com>
2024-04-30 05:37:53 +0800

    [file   ]: codeforce template
    Signed-off-by: Bo-Wei Chen(BWbwchen) <tim.chenbw@gmail.com>

---
+++ codeforce/template.cpp
@@ -0,0 +1,23 @@
+#define MAXN 2000000000
+#define llu unsigned long long
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+#ifdef DEBUG
+    freopen("in.in", "r", stdin);
+#endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    // do something
+
+    return 0;
+}
